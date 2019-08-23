28d0c8c5a17d3e7788f267874490172512e26fbb
macymuhia <macymuhia@gmail.com>
2019-08-23 15:56:39 +0300

    Add gallery app to project settings, add routes and corresponding view functions

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import request
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def gallery(request):
+    return render(request, "gallery.html")
+
