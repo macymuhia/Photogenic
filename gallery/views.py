be0d6ef5f7a9584f6b972b002e028f5ae9eab786
macymuhia <macymuhia@gmail.com>
2019-08-27 12:40:22 +0300

    Displaying name of category searched

--- gallery/views.py
+++ gallery/views.py
@@ -17,15 +17,16 @@ def gallery(request):
 
 def gallery_category(request, category_id):
     pics = Image.fetch_images_in_category(category_id)
+    # cat_name = pics.category_id.name
     return render(request, "category.html", {"pics": pics})
 
 
-def search_category(request):
-    search_term = request.GET.get("category")
-    print(search_term)
-    pics = Image.search_by_category(search_term)
-    print(pics)
-    return render(request, "search.html", {"pics": pics})
+# def search_category(request):
+#     search_term = request.GET.get("category")
+#     print(search_term)
+#     pics = Image.search_by_category(search_term)
+#     print(pics)
+#     return render(request, "search.html", {"pics": pics})
 
 
 def pic_location(request, location_id):
@@ -39,18 +40,18 @@ def pic_location(request, location_id):
     )
 
 
-# def search_results(request):
+def search_category(request):
 
-#     if "category" in request.GET and request.GET["category"]:
-#         search_term = request.GET.get("category")
-#         searched_categories = Image.search_by_category(search_term)
-#         message = f"{search_term}"
+    if "category" in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_categories = Image.search_by_category(search_term)
+        message = f"{search_term}"
 
-#         return render(
-#             request, "search.html", {"message": message, "pics": searched_categories}
-#         )
+        return render(
+            request, "search.html", {"message": message, "pics": searched_categories}
+        )
 
-#     else:
-#         message = "You haven't searched for any category"
-#         return render(request, "category.html", {"message": message})
+    else:
+        message = "You haven't searched for any category"
+        return render(request, "category.html", {"message": message})
 
