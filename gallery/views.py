0dc3025cfe09c135da485c847da8052484455958
macymuhia <macymuhia@gmail.com>
2019-08-28 08:17:39 +0300

    Add copy to clipboard functionality

--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import request
 from .models import Image, Category, Location
 
+# import pyperclip
+
 # Create your views here.
 def index(request):
     return render(request, "index.html")
@@ -18,8 +20,11 @@ def gallery(request):
 def gallery_category(request, category_id):
     pics = Image.fetch_images_in_category(category_id)
     icon = Image.icon
-    # cat_name = pics.category_id.name
-    return render(request, "category.html", {"pics": pics, "icon": icon})
+    # copy = pyperclip.copy(request.GET.get("copy"))
+    cat_name = Image.category
+    return render(
+        request, "category.html", {"pics": pics, "icon": icon, "cat_name": cat_name}
+    )
 
 
 # def search_category(request):
@@ -32,13 +37,16 @@ def gallery_category(request, category_id):
 
 def pic_location(request, location_id):
     all_locations = Location.get_all()
+    loc_name = Image.location
 
     if location_id == 00:
         pics = Image.all_images()
     else:
         pics = Image.filter_by_location(location_id)
     return render(
-        request, "location.html", {"pics": pics, "all_locations": all_locations}
+        request,
+        "location.html",
+        {"pics": pics, "all_locations": all_locations, "loc_name": loc_name},
     )
 
 
