ac348be9780db7fafa0a684fa80b7c32d94fcf5d
macymuhia <macymuhia@gmail.com>
2019-09-04 12:20:02 +0300

    Deployment

--- panorama/settings.py
+++ panorama/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
